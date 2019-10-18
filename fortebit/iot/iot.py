4ff1a14b889d35cb66bd2a15d2dcd831b6f685d5
ppescher <p.messina@robotechsrl.com>
2019-10-18 18:33:23 +0200

    Add updated Fortebit and Polaris libraries

---
+++ fortebit/iot/iot.py
@@ -0,0 +1,131 @@
+"""
+.. module:: iot
+
+********************
+Fortebit IoT Library
+********************
+
+The Zerynth Fortebit IoT Library can be used to ease the connection to the `Fortebit IoT Cloud <https://fortebit.tech/cloud/>`_.
+
+It makes your device act as a Fortebit IoT Device that can be monitored and controlled on the Fortebit IoT Cloud dashboard.
+
+The device always send and receive data in the JSON format.
+
+    """
+
+
+class Device():
+    """
+================
+The Device class
+================
+
+.. class:: Device(device_token, client, ctx=None)
+
+        Create a Device instance representing a Fortebit IoT device.
+
+        The device is provisioned by the :samp:`device_token`, obtained from the Fortebit dashboard upon the creation of a new device.
+        The :samp:`client` parameter is a class that provides the implementation of the low level details for the connection.
+        It can be one of :samp:`MqttClient` in the :samp:`mqtt_client` module, or :samp:`HttpClient` in the :samp:`http_client` module.
+        The optional :samp:`ctx` parameter is an initialized secure socket context.
+
+    """
+
+    def __init__(self, device_token, client, ctx=None):
+        self.device_token = device_token
+        self.endpoint = "cloud.fortebit.tech"
+        self.client = client(self.endpoint, device_token, ctx)
+
+    def listen_rpc(self, callback):
+        """
+.. method:: listen_rpc(callback)
+
+        Listen to incoming RPC requests that get reported to the specified :samp:`callback` function,
+        called as *callback(id, method, params)*:
+        
+        * :samp:`id` is the request identifier (number)
+        * :samp:`method` is the method identifier of the RPC (Remote Procedure Call)
+        * :samp:`params` is a dictionary containing the RPC method arguments (or parameters)
+        
+        Call :func:`send_rpc_reply` to provide the result of the RPC request.
+        
+        """
+        self.client.on_rpc_request = callback
+
+    def connect(self, retry=7):
+        """
+.. method:: connect()
+
+        Setup a connection to the Fortebit Cloud. It can raise an exception in case of error.
+
+        """
+        if self.client.connect():
+            return True
+        t = 3000
+        while (retry > 0):
+            sleep(t)
+            if self.client.connect():
+                return True
+            # increase (double) the delay between attempts (up to 1 minute)
+            if t < 30000:
+                t *= 2
+            retry -= 1
+        return False
+
+    def is_connected(self):
+        """
+.. method:: is_connected()
+
+        Returns the status of the connection to the Fortebit Cloud (reconnections are automatic).
+
+        """
+        return self.client.is_connected()
+
+    def run(self):
+        """
+.. method:: run()
+
+        Starts the device by executing the underlying client. It can start a new thread depending on the type of client (Mqtt vs Http)
+
+        """
+        self.client.loop()
+
+    def publish_telemetry(self, values, ts=None):
+        """
+.. method:: publish_telemetry(values, ts)
+
+        Publish :samp:`values` (dictionary) to the device telemetry, with optional timestamp :samp:`ts` (epoch in milliseconds).
+
+        Return a boolean, *False* if the message cannot be sent.
+        """
+        return self.client.publish_telemetry(values, ts)
+
+    def publish_attributes(self, attributes):
+        """
+.. method:: publish_attributes(attributes)
+
+        Publish :samp:`attributes` (dictionary) to the device *client* attributes.
+
+        Return a boolean, *False* if the message cannot be sent.
+        """
+        return self.client.publish_attributes(attributes)
+
+    def get_attributes(self, client, shared=None, timeout=10000):
+        """
+.. method:: get_attributes(client, shared, timeout)
+
+        Obtain the specified :samp:`client` and/or :samp:`shared` attributes from the device.
+
+        Return a dictionary, *None* if the data could not be received.
+        """
+        return self.client.get_attributes(client, shared, timeout)
+
+    def send_rpc_reply(self, id, result):
+        """
+.. method:: send_rpc_reply(id, result)
+
+        Publish :samp:`result` (dictionary) as a reply to the RPC request with identifier :samp:`id`.
+
+        Return a boolean, *False* if the message cannot be sent.
+        """
+        return self.client.send_rpc_reply(id, result)
---
+++ fortebit/iot/mqtt_client.py
@@ -0,0 +1,137 @@
+from lwmqtt import mqtt
+import json
+import streams
+import threading
+import mcu
+
+debug = False
+
+
+def print_d(*args):
+    if debug:
+        print(*args)
+
+
+class MqttClient():
+
+    def __init__(self, endpoint, device_token, ctx=None):
+        self.endpoint = endpoint
+        self.ctx = ctx
+        self.request_id = -1
+        self.rpc_id = 10000
+        self.driver = mqtt.Client("polaris", True)
+        self.driver.set_username_pw(device_token)
+        self.attr_ev = threading.Event()
+        self.attr_obj = None
+
+    def _loop_failure(self, client):
+        while True:
+            try:
+                print_d("> reconnecting...")
+                client.reconnect()
+                break
+            except Exception as e:
+                print_d(e)
+            sleep(10000)
+        return mqtt.RECOVERED
+
+    def _subscribe_cb(self, client):
+        # subscribe to attributes
+        client.subscribe("v1/devices/me/attributes/response/+", self._on_attributes, 1)
+        client.subscribe("v1/devices/me/rpc/request/+", self._on_rpc_request, 1)
+
+    def connect(self):
+        port = 1883 if self.ctx is None else 8883
+        try:
+            self.driver.connect(self.endpoint, 60, port=port, ssl_ctx=self.ctx,
+                                loop_failure=self._loop_failure, aconnect_cb=self._subscribe_cb, start_loop=False)
+        except Exception as e:
+            print_d("failed", e)
+            return False
+        return True
+
+    def is_connected(self):
+        return self.driver.connected()
+
+    def loop(self):
+        self.driver.loop()
+
+    def _on_attributes(self, client, payload, topic):
+        print_d("> got:", topic, payload)
+        # len("v1/devices/me/attributes/response/") = 34
+        id = int(topic[34:])
+        if self.request_id == id:
+            self.attr_obj = json.loads(payload)
+            self.attr_ev.set()
+
+    def on_rpc_request(self, id, method, params):
+        pass
+
+    def _on_rpc_request(self, client, payload, topic):
+        print_d("> got:", topic, payload)
+        # len("v1/devices/me/rpc/request/") = 26
+        id = int(topic[26:])
+        obj = json.loads(payload)
+        self.on_rpc_request(id, obj['method'], obj['params'] if 'params' in obj else None)
+
+    def get_attributes(self, client, shared=None, timeout=10000):
+        self.request_id += 1
+        self.attr_ev.clear()
+        obj = {}
+        if client and isinstance(client, PSTRING):
+            obj['clientKeys'] = client
+        else:
+            client = None
+        if shared and isinstance(shared, PSTRING):
+            obj['sharedKeys'] = shared
+        else:
+            shared = None
+        obj = json.dumps(obj)
+        ep = 'v1/devices/me/attributes/request/' + str(self.request_id)
+        print_d("publish:", ep, obj)
+        try:
+            self.driver.publish(ep, obj, qos=1)
+            obj = None
+            self.attr_ev.wait(timeout)
+            obj = self.attr_obj
+        except Exception as e:
+            print_d(e)
+            return None
+        if obj is None:
+            return None
+        if 'client' in obj:
+            client = obj['client']
+        else:
+            client = None
+        if 'shared' in obj:
+            shared = obj['shared']
+        else:
+            shared = None
+        return (client, shared)
+
+    def publish_attributes(self, attributes):
+        try:
+            self.driver.publish('v1/devices/me/attributes', json.dumps(attributes), qos=1)
+        except Exception as e:
+            print_d(e)
+            return False
+        return True
+
+    def publish_telemetry(self, values, ts=None):
+        if ts is not None:
+            values = {'values': values, 'ts': ts}
+        try:
+            self.driver.publish("v1/devices/me/telemetry", json.dumps(values), qos=1)
+        except Exception as e:
+            print_d(e)
+            return False
+        return True
+
+    def send_rpc_reply(self, id, result):
+        print_d("rpc reply", id, result)
+        try:
+            self.driver.publish("v1/devices/me/rpc/response/" + str(id), json.dumps(result), qos=1)
+        except Exception as e:
+            print_d(e)
+            return False
+        return True
